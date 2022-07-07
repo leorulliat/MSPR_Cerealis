4956b81002be21239c82d15b35581995ea8eef06
Léo Rulliat <leorulliat@gmail.com>
2022-07-07 22:08:00 +0200

    ajout commentaires unit tests

--- test/test.js
+++ test/test.js
@@ -7,6 +7,15 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('API CEREALIS', () => {
+
+    /**
+     * DELETE/all
+     * on commence par supprimer tous les utilisateurs
+     * expected : 
+     *  statut = 200 (OK)
+     *  type = string
+     *  text = "DB CLEAR" 
+     */
     describe('DELETE /all', () => {
         it("It should DELETE all existing users", (done) => {
             chai.request(app)
@@ -20,6 +29,19 @@ describe('API CEREALIS', () => {
         });
     });
 
+    /**
+     * POST/registerUser
+     * on ajoute un utilisateur :
+     * {
+     *    email: "andres.boulanger@epsi.fr",
+     *    firstName: "Andres",
+     *    lastName: "Boulanger"
+     * }
+     * expected : 
+     *  statut = 201 (created)
+     *  type = string
+     *  text = "Enregistré" 
+     */
     describe('POST /registerUser', () => {
         it('It should POST a new user and get the message : "Enregistré"', (done) => {
             const user = {
@@ -40,6 +62,14 @@ describe('API CEREALIS', () => {
         });
     });
 
+    /**
+     * GET/getCount
+     * on vérifie que le nb d'utilisateurs soit à 1
+     * expected : 
+     *  statut = 200 (OK)
+     *  type = object 
+     *  value = { count : 1 }
+     */
     describe('GET /getCount', () => {
         it("It should GET the number of users (1)", (done) => {
             chai.request(app)
@@ -55,6 +85,14 @@ describe('API CEREALIS', () => {
         });
     });
 
+    /**
+     * GET/getAll
+     * On récupère tous les utilisateurs
+     * expected : 
+     *  statut = 200 (OK)
+     *  type = array
+     *  length = 1
+     */
     describe('GET /getAll', () => {
         it("It should GET all users (array of 1 user)", (done) => {
             chai.request(app)
@@ -68,6 +106,19 @@ describe('API CEREALIS', () => {
         });
     });
 
+    /**
+     * POST/registerUser
+     * on ajoute un utilisateur :
+     * {
+     *    email: "andres.boulanger@epsi.fr",
+     *    firstName: "Andres",
+     *    lastName: "Boulanger"
+     * }
+     * expected : 
+     *  statut = 200 (OK)
+     *  type = string
+     *  text = "Déjà enregistré" 
+     */
     describe('POST /registerUser', () => {
         it('It should try to POST a the same user and get the message : "Déjà enregistré"', (done) => {
             const user = {
@@ -88,6 +139,14 @@ describe('API CEREALIS', () => {
         });
     });
 
+    /**
+     * DELETE/all
+     * on termine par supprimer l'utilisateur
+     * expected : 
+     *  statut = 200
+     *  type = string
+     *  text = "DB CLEAR" 
+     */
     describe('DELETE /all', () => {
         it("It should DELETE the user", (done) => {
             chai.request(app)
@@ -99,8 +158,16 @@ describe('API CEREALIS', () => {
                     done();
                 });
         });
-    });    
+    });  
 
+    /**
+     * GET/getCount
+     * on vérifie que le nb d'utilisateurs à 0
+     * expected : 
+     *  statut = 200 (OK)
+     *  type = object 
+     *  value = { count : 0 }
+     */
     describe('GET /getCount', () => {
         it("It should GET Zero user", (done) => {
             chai.request(app)
